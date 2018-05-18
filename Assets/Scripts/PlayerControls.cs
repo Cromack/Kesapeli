f19d590a9ab1831b566c53ac527cf97e7cd15582
Vili Saura <vili.saura@student.tut.fi>
2018-05-18 18:14:32 +0300

    Pelaajan kontrollointi lisätty

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public int speed;
+    Animator animator;
+
+    enum State{
+        Idle,
+        Walking,
+        Combat
+    };
+    
+    State CurrentState = State.Idle;
+
+    void ChangeState()
+    {
+        switch (CurrentState)
+        {
+            case State.Idle:
+                if (Input.GetButtonDown("Horizontal") == true)
+                {
+                    CurrentState = State.Walking;
+                    animator.SetBool("Walking", true);
+                    //animator.SetTrigger("Attack");
+                }
+                break;
+
+            case State.Walking:
+
+                float accelerate = Input.GetAxis("Horizontal");
+                Vector2 acceleration = new Vector2(accelerate, 0);
+                rb.AddForce(acceleration * speed);
+
+                if (Input.GetButton("Horizontal") == false)
+                {
+                    CurrentState = State.Idle;
+                    animator.SetBool("Walking", false);
+                }
+
+                break;
+        }
+            
+    }
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ChangeState();
+    }
+}
